a15f336b84dc228d10d0ae18cb99d67020794dfb
asamoto1983 <asamoto1983@gmail.com>
2022-07-28 09:16:45 +0900

    バリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,17 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
-  validates :email,                   presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
-  validates :encrypted_password,      presence: true, length: { minimum: 6 }
+   
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  VALID_NAME = /\A[ぁ-んァ-ヶ一-龥々ー]+\z/
+  VALID_KANA = /\A[ァ-ヶー]+\z/
+
+  validates :password,                presence: true, format: { with: VALID_PASSWORD_REGEX , message: 'には英字と数字の両方を含めて設定してください'}
   validates :nickname,                presence: true
-  validates :first_name,              presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
-  validates :last_name,               presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
-  validates :first_name_kana,         presence: true, format: { with: /\A[ァ-ヶ]+\z/ }
-  validates :last_name_kana,          presence: true, format: { with: /\A[ァ-ヶ]+\z/ }
+  validates :first_name,              presence: true, format: { with: VALID_NAME , message: '全角文字を使用してください'}
+  validates :last_name,               presence: true, format: { with: VALID_NAME , message: '全角文字を使用してください'}
+  validates :first_name_kana,         presence: true, format: { with: VALID_KANA , message: '全角カタカナを使用してください'}
+  validates :last_name_kana,          presence: true, format: { with: VALID_KANA , message: '全角カタカナを使用してください'}
   validates :birth_day,               presence: true
 
-  has_many :buys
   has_many :products
   
 end
