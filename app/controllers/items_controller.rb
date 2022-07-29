5b8b19f0f53c4bbd404c05d66382ed89910a8633
asamoto1983 <asamoto1983@gmail.com>
2022-07-29 15:43:13 +0900

    商品出品機能を実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,17 +1,26 @@
 class ItemsController < ApplicationController
+
+
   def index
-    
   end
 
   def new
     @item = Item.new
   end
 
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
 
 
-  #private
+  private
 
-  #def item_params
-  #  params.require(:item).permit(:image).merge(user_id: current_user.id)
-  #end
+  def item_params
+    params.require(:item).permit(:image,:item,:price, :explain, :category_id, :condition_id, :del_charge_id,:prefecture_id,:del_day_id).merge(user_id: current_user.id)
+  end
 end
---
+++ app/javascript/item_price.js
@@ -0,0 +1,11 @@
+window.addEventListener('load', () => {
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    const commission = Math.floor(inputValue/ 10);
+    const addTaxDom = document.getElementById("add-tax-price");
+    addTaxDom.innerHTML = commission
+    const SalesProfit = document.getElementById("profit");
+    SalesProfit.innerHTML = inputValue - commission;
+  });
+});
