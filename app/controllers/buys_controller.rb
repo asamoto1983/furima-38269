748d5dc8abbd8adcf34fbbca8c3d6886b3152f97
asamoto1983 <asamoto1983@gmail.com>
2022-08-03 07:05:19 +0900

    購入機能を実装

---
+++ app/controllers/buys_controller.rb
@@ -0,0 +1,43 @@
+class BuysController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_item, only: [:index, :create]
+
+  def index
+    if user_signed_in? && current_user.id != @item.user_id && @item.buy.nil?
+      @buy_address = BuyAddress.new
+    else
+      redirect_to root_path
+    end
+  end
+
+  def create
+    @buy_address = BuyAddress.new(buy_params)
+    if @buy_address.valid?
+      pay_item
+      @buy_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def buy_params
+    params.require(:buy_address).permit(:post_code, :prefecture_id, :municipalities,:house_number, :build_name,
+                                         :phone_number).merge(user_id: current_user.id,item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: buy_params[:token],
+      currency: 'jpy'
+    )
+  end
+end
--- app/models/buy_address.rb
+++ app/models/buy_address.rb
@@ -1,3 +1,23 @@
 class BuyAddress
+  
   include ActiveModel::Model
+  attr_accessor :token, :user_id, :item_id, :post_code, :prefecture_id, :municipalities, :house_number, :build_name, :phone_number
+
+  with_options presence: true do
+    validates :user_id
+    validates :item_id
+    validates :token
+    validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
+    validates :municipalities, :house_number
+    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'は半角数値のみ登録可能です' }
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+  end
+  
+
+  def save
+    buy = Buy.create(user_id: user_id, item_id: item_id)
+    Address.create(post_code: post_code, prefecture_id: prefecture_id, municipalities: municipalities,
+                   house_number: house_number, build_name: build_name, phone_number: phone_number, buy_id: buy.id)
+  end
+
 end
